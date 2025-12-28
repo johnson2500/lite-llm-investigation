0a3e7e93ba5352d4b8618ae4ea35e0fb7268e92c
Hadar Cohen <hacohen@redhat.com>
2025-12-28 14:38:05 +0200

    run llamastack with litellm backend

---
+++ demos/llamastack_test.py
@@ -0,0 +1,20 @@
+from llama_stack_client import LlamaStackClient
+
+client = LlamaStackClient(
+    base_url="https://litellm-hacohen-llmlite.apps.ai-dev02.kni.syseng.devcluster.openshift.com",  # Your LiteLLM URL
+    api_key= "master-key"
+)
+
+# 1. List available models (Proves LiteLLM is forwarding the model list)
+models = client.models.list()
+print("Available Models:", models)
+
+# 2. Run an Inference Call
+response = client.chat.completions.create(
+    model="llama-fp8", # LiteLLM maps this to your local Llama 3
+    messages=[
+        {"role": "user", "content": "Hello! Are you compatible with Llama Stack?"}
+    ]
+)
+
+print("\nResponse:", response.choices[0].message.content)
